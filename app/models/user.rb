708f862df7fe6490cfe1944f144d694f35188170
kohei.baba <kbforanypurpose@gmail.com>
2019-03-06 10:30:09 +0000

    for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,27 @@ class User < ApplicationRecord
   end
   
   def unwant(item)
-    want = self.wants.find_by(item_id item.id)
+    want = self.wants.find_by(item_id: item.id)
     want.destroy if want
   end
   
   def want?(item)
     self.want_items.include?(item)
+  end
+
+  has_many :haves, class_name: 'Have'
+  has_many :have_items, through: :haves, source: :item
+  
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+  
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id )
+    have.destroy if have
+  end
+  
+  def have?(item)
+    self.have_items.include?(item)
   end
 end
